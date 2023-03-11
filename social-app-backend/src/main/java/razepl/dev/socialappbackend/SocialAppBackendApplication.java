c29819af8d3b3268ea480ce7ced44d5003053a12
Szymon Półtorak <szymonpotorak@gmail.com>
2023-03-11 21:02:33 +0100

    Initialized backend

---
+++ social-app-backend/src/main/java/razepl/dev/socialappbackend/SocialAppBackendApplication.java
@@ -0,0 +1,11 @@
+package razepl.dev.socialappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialAppBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SocialAppBackendApplication.class, args);
+    }
+}
