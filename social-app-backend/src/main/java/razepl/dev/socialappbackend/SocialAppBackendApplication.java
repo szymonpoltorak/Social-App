99a772e6ae5306c0c49b383d6387124222a66fb1
Szymon Półtorak <szymonpotorak@gmail.com>
2023-03-12 10:52:57 +0100

    Added and configured logback

--- social-app-backend/src/main/java/razepl/dev/socialappbackend/SocialAppBackendApplication.java
+++ social-app-backend/src/main/java/razepl/dev/socialappbackend/SocialAppBackendApplication.java
@@ -1,11 +1,15 @@
 package razepl.dev.socialappbackend;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class SocialAppBackendApplication {
     public static void main(String[] args) {
+        log.info("Starting an adventure!");
+
         SpringApplication.run(SocialAppBackendApplication.class, args);
     }
 }
