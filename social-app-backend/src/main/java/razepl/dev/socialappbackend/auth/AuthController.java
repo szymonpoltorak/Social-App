54f9582e65c63dfa8af960ea653ad42c188e22cf
Szymon Poltorak <szymonpotorak@gmail.com>
2023-04-10 16:11:21 +0200

    Added exception handling for AuthController

--- social-app-backend/src/main/java/razepl/dev/socialappbackend/auth/AuthController.java
+++ social-app-backend/src/main/java/razepl/dev/socialappbackend/auth/AuthController.java
@@ -1,10 +1,14 @@
 package razepl.dev.socialappbackend.auth;
 
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import razepl.dev.socialappbackend.user.ServiceUser;
 import razepl.dev.socialappbackend.user.UserRepository;
+
+import java.util.stream.Collectors;
 
 import static razepl.dev.socialappbackend.auth.constants.AuthMappings.AUTH_MAPPING;
 import static razepl.dev.socialappbackend.auth.constants.AuthMappings.REGISTER_MAPPING;
@@ -23,9 +27,20 @@ public class AuthController implements AuthInterface {
 
     @Override
     @PostMapping(value = REGISTER_MAPPING)
-    public ResponseEntity<String> registerUser(@RequestBody ServiceUser user) {
+    public ResponseEntity<String> registerUser(@Valid @RequestBody ServiceUser user) {
         userRepository.save(user);
 
         return ResponseEntity.ok("User has been successfully registered!");
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<String> handleValidationExceptions(ConstraintViolationException exception) {
+        String errorMessage = exception.getConstraintViolations().stream()
+                .map(error -> String.format("%s : %s", error.getPropertyPath(), error.getMessage()))
+                .collect(Collectors.joining("\n"));
+
+        log.error(errorMessage);
+
+        return ResponseEntity.badRequest().body(errorMessage);
     }
 }
