0a15f62a79a337a764f083f140ad6382a68fdf61
Szymon Półtorak <szymonpotorak@gmail.com>
2023-05-03 17:40:17 +0200

    Finished refactoring

--- social-app-frontend/src/app/app.module.ts
+++ social-app-frontend/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HTTP_INTERCEPTORS, HttpClientModule } from "@angular/common/http";
-import { AuthInterceptor } from "./core/interceptors/auth.interceptor";
+import { AuthInterceptor } from "@core/interceptors/auth.interceptor";
 
 
 @NgModule({
