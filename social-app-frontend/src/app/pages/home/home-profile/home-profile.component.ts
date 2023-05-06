5383648ea7066485164eb481d4af5de7595bca5b
Szymon Półtorak <szymonpotorak@gmail.com>
2023-05-06 20:50:41 +0200

    Added userdata rendering on the home page

--- social-app-frontend/src/app/pages/home/home-profile/home-profile.component.ts
+++ social-app-frontend/src/app/pages/home/home-profile/home-profile.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { HomeService } from "@core/services/home.service";
+import { UserHomeDataService } from "@core/services/user-home-data.service";
+import { UtilService } from "@core/services/util.service";
+import { StorageKeys } from "@core/enums/StorageKeys";
+import { Subject, takeUntil } from "rxjs";
+import { UserData } from "@core/interfaces/home/UserData";
 
 @Component({
     selector: 'app-home-profile',
@@ -7,12 +12,39 @@ import { HomeService } from "@core/services/home.service";
     styleUrls: ['./home-profile.component.scss']
 })
 export class HomeProfileComponent implements OnInit, OnDestroy {
-    constructor(private homeService: HomeService) {
+    job!: string;
+    userLocation!: string;
+    numOfFriends!: number;
+    fullName !: string;
+    twitter: string = "";
+    github: string = "";
+    linkedin: string = "";
+    private userDataDestroy$: Subject<any> = new Subject<any>();
+
+    constructor(private homeService: HomeService,
+                private userDataService: UserHomeDataService,
+                private utilService: UtilService) {
     }
 
     ngOnInit(): void {
+        const username: string = this.utilService.getValueFromStorage(StorageKeys.USERNAME);
+
+        this.homeService.getUserData(username.substring(1, username.length - 1))
+            .pipe(takeUntil(this.userDataDestroy$))
+            .subscribe((data: UserData): void => {
+                console.log(data);
+
+                this.job = data.job;
+                this.fullName = data.fullName;
+                this.numOfFriends = data.numOfFriends;
+                this.userLocation = data.location;
+                this.twitter = data.twitter;
+                this.github = data.github;
+                this.linkedin = data.linkedin;
+            });
     }
 
     ngOnDestroy(): void {
+        this.userDataDestroy$.complete();
     }
 }
