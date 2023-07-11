3fa09b55663789d45757a3a810c84ef35040f751
Szymon Półtorak <szymonpotorak@gmail.com>
2023-07-11 22:59:48 +0200

    Added working success handler

--- social-app-backend/src/main/java/razepl/dev/socialappbackend/config/oauth/OAuthSuccessHandler.java
+++ social-app-backend/src/main/java/razepl/dev/socialappbackend/config/oauth/OAuthSuccessHandler.java
@@ -6,16 +6,18 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import razepl.dev.socialappbackend.config.jwt.interfaces.JwtServiceInterface;
+import razepl.dev.socialappbackend.config.oauth.interfaces.IOAuthSuccessHandler;
 
 import java.io.IOException;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
-public class OAuthSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+public class OAuthSuccessHandler extends SimpleUrlAuthenticationSuccessHandler implements IOAuthSuccessHandler {
     private final JwtServiceInterface jwtService;
 
     @Override
@@ -36,16 +38,13 @@ public class OAuthSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
         log.error("Principal : {}", authentication.getPrincipal());
         log.error("Credentials : {}", authentication.getCredentials());
 
-//        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
 
-//        String token = jwtService.generateToken(userDetails);
-//        String refreshToken = jwtService.generateRefreshToken(userDetails);
+        String token = jwtService.generateToken(userDetails);
+        String refreshToken = jwtService.generateRefreshToken(userDetails);
 
-//        response.addCookie(cookieUtils.buildAuthTokenCookie(token));
-//        response.addCookie(cookieUtils.buildRefreshTokenCookie(refreshToken));
-//        response.addCookie(cookieUtils.buildUserCookie((UserDetails) authentication.getPrincipal()));
-//        response.addHeader("AuthToken", token);
-//        response.addHeader("RefreshToken", refreshToken);
+        response.addHeader("AuthToken", token);
+        response.addHeader("RefreshToken", refreshToken);
 
         response.sendRedirect(targetUrl);
     }
---
+++ social-app-backend/src/main/java/razepl/dev/socialappbackend/config/oauth/interfaces/IOAuthSuccessHandler.java
@@ -0,0 +1,14 @@
+package razepl.dev.socialappbackend.config.oauth.interfaces;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import java.io.IOException;
+
+public interface IOAuthSuccessHandler extends AuthenticationSuccessHandler {
+    void handle(HttpServletRequest request,
+                HttpServletResponse response,
+                Authentication authentication) throws IOException;
+}
